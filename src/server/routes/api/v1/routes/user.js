7779480b77a673957d307de89b6acc74a74913db
IceBroForever <pasha1ruh@gmail.com>
2018-02-03 20:57:25 +0200

    API companies of user

--- src/server/routes/api/v1/routes/user.js
+++ src/server/routes/api/v1/routes/user.js
@@ -5,6 +5,7 @@ const Utils = require('@utils');
 const passport = require('passport');
 
 const UserDB = require('@DBfolder/users');
+const CompanyDB = require('@DBfolder/company');
 
 router.get('/:id', async (req, res, next) => {
     let user = await UserDB.get.byID(req.params.id);
@@ -13,6 +14,19 @@ router.get('/:id', async (req, res, next) => {
         return res.json({
             success: true,
             item: user.info()
+        });
+    } else {
+        return Utils.sendError(res, 404, 'Not found');
+    }
+});
+
+router.get('/:id/companies', async (req, res, next) => {
+    let user = await UserDB.get.byID(req.params.id);
+
+    if (user) {
+        return res.json({
+            success: true,
+            item: await CompanyDB.find.byAdmin(user.id)
         });
     } else {
         return Utils.sendError(res, 404, 'Not found');
