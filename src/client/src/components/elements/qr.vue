60239b4901f6fe7d3cd6e4fd10e6d9c700b00ff0
Danil <danil.kazim.99@gmail.com>
2018-02-04 01:46:07 +0200

    qr user

--- src/client/src/components/elements/qr.vue
+++ src/client/src/components/elements/qr.vue
@@ -1,24 +1,28 @@
 <template lang="pug">
-    div.container
-        qrcode-vue(:value="link",  :size="qrsize", :level="H")
-
+  qrcode-vue(:value="link",  :size="size", :level="H")
 </template>
 
 <script>
-    import QrcodeVue from 'qrcode.vue';
+  import QrcodeVue from 'qrcode.vue';
 
-    export default {
-        name: "QR",
-        data() {
-            return {
-                link: 'http://google.ua',
-                qrsize: 300
-            }
-        },
-        components: {
-            QrcodeVue
-        }
+  export default {
+    name: "QR",
+    data () {
+      return {}
+    },
+    components: {
+      QrcodeVue
+    },
+    props: {
+      link: {
+        required: true,
+        type: String
+      },
+      size: {
+        default: 200
+      }
     }
+  }
 </script>
 <style scoped lang="scss">
 </style>
--- src/client/src/components/elements/user/UserFull.vue
+++ src/client/src/components/elements/user/UserFull.vue
@@ -1,27 +1,41 @@
 <template lang="pug">
-  div.box
-    figure.image.avatar.is-256x256
-      img.avatar-image(:src="user.picture||'/static/img/user.png'")
-    div.content
-      section
-        b-field(label="Ім'я")
-          b-input(:value="user.name", readonly, icon="account")
-        b-field(label="ID")
-          b-input(:value="user.id||user._id", readonly, icon="number")
-        b-field(label="Електрона адреса")
-          b-input(:value="user.email", readonly, icon="email")
-        b-field(label="Зареєстрований")
-          b-input(:value="joined", readonly, icon="calendar-today")
-        b-field(v-if="user.role=='admin'",label="Адміністатор")
-          b-switch(:value="user.role=='admin'",readonly="true", disabled)
+  div
+    div.box
+      figure.image.avatar.is-256x256
+        img.avatar-image(:src="user.picture||'/static/img/user.png'")
+      br
+      div.has-text-centered
+        figure.image.is-128x128#qr
+          qr(:link="userLink", size="128")
+      div.content
+        section
+          b-field(label="Ім'я")
+            b-input(:value="user.name", readonly, icon="account")
+          b-field(label="ID")
+            b-input(:value="user.id||user._id", readonly, icon="number")
+          b-field(label="Електрона адреса")
+            b-input(:value="user.email", readonly, icon="email")
+          b-field(label="Зареєстрований")
+            b-input(:value="joined", readonly, icon="calendar-today")
+          b-field(v-if="user.role=='admin'",label="Адміністатор")
+            b-switch(:value="user.role=='admin'",readonly="true", disabled)
 </template>
 <script>
+  import URLJoin from 'url-join';
+  import Qr from '../qr';
+
   export default {
     data () {
       return {}
     },
+    components: {
+      Qr
+    },
     methods: {},
     computed: {
+      userLink () {
+        return URLJoin(this.$config.ROOT_URL, '/#', '/user', this.user._id || this.user.id);
+      },
       joined () {
         return (new Date(this.user.created)).toLocaleString();
       }
@@ -49,6 +63,10 @@
     border: 5px white solid;
     box-sizing: content-box;
     box-shadow: 0 2px 3px rgba(10, 10, 10, .1), 0 0 0 1px rgba(10, 10, 10, .1);
+  }
+
+  #qr {
+    margin: auto;
   }
 
   .box {
