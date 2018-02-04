2071a046ef2fbb67ff8132983591e643708afdc0
Danil <danil.kazim.99@gmail.com>
2018-02-04 10:14:57 +0200

    rebuild

--- src/client/src/config.js
+++ src/client/src/config.js
@@ -1,11 +1,11 @@
 let config = {};
 config.IS_DEV = process.env.NODE_ENV.startsWith('dev');
 config.ROOT_URL = window.location.host;
-config.SERVER_URL = config.IS_DEV ? 'http://localhost:3000' : config.ROOT_URL;
+config.SERVER_URL = config.IS_DEV ? 'http://localhost:3000' : '/';
 config.auth = {
   facebook: {
     APP_ID: '149504242426780',
-    URL: 'api/v1/auth/facebook',
+    URL: '/api/v1/auth/facebook',
     SCOPE: "email",
   }
 };
